d801a9c8d7bd843dfa8011994c104803b8c11408
Your Name <you@example.com>
2023-02-08 12:14:01 +0400

    any changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,20 @@ $(document).ready(function () {
     $(".button-body1").slideToggle("slow");
     $(".button-body2").slideUp("slow");
     $(".button-body3").slideUp("slow");
+    $(".accor1 i").toggleClass("active");
   });
   $(".accor2").click(function () {
     $(".button-body2").slideToggle("slow");
     $(".button-body1").slideUp("slow");
     $(".button-body3").slideUp("slow");
+    $(".accor2 i").toggleClass("active");
+
   });
   $(".accor3").click(function () {
     $(".button-body3").slideToggle("slow");
     $(".button-body1").slideUp("slow");
     $(".button-body2").slideUp("slow");
+    $(".accor3 i").toggleClass("active");
+
   });
 });
